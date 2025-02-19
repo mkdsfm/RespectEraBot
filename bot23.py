32502698f1199ba3fd55246f276883558730a856
Irina Tiulkina <irina.tyulkina10@mail.ru>
2025-02-20 01:25:18 +0300

    [add] картинки

--- bot23.py
+++ bot23.py
@@ -5,6 +5,7 @@ import os
 import json
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.types import FSInputFile
 
 # Твой токен бота (замени на реальный)
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -15,6 +16,11 @@ with open("config.json", "r", encoding="utf-8") as f:
 
 GENERAL_COMPLIMENTS = config.get("GENERAL_COMPLIMENTS", [])
 PERSONAL_COMPLIMENTS = config.get("PERSONAL_COMPLIMENTS", {})
+
+IMAGE_DIR = "images"
+
+IMAGE_PATHS = [os.path.join(IMAGE_DIR, file) for file in os.listdir(IMAGE_DIR) if os.path.isfile(os.path.join(IMAGE_DIR, file))]
+
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -38,16 +44,23 @@ def is_feminine(name: str) -> bool:
 async def send_compliment(message: types.Message):
     username = message.from_user.username
     first_name = message.from_user.first_name or "Товарищ"
-    if is_feminine(first_name) and f"@{username}" not in PERSONAL_COMPLIMENTS:
-        return  # Пропустить отправку комплимента для этих пользователей
-    
-    if username and f"@{username}" in PERSONAL_COMPLIMENTS:
-        compliment = random.choice(PERSONAL_COMPLIMENTS[f"@{username}"]).format(first_name=first_name)
-    else:
-        compliment = random.choice(GENERAL_COMPLIMENTS).format(first_name=first_name)
-    
-    await message.answer(compliment)
 
+    if is_feminine(first_name) and f"@{username}" not in PERSONAL_COMPLIMENTS:
+            return  # Пропустить отправку комплимента для этих пользователей
+    
+    if random.choice([True, False]):    
+        if username and f"@{username}" in PERSONAL_COMPLIMENTS:
+            compliment = random.choice(PERSONAL_COMPLIMENTS[f"@{username}"]).format(first_name=first_name)
+        else:
+            compliment = random.choice(GENERAL_COMPLIMENTS).format(first_name=first_name)
+
+        await message.answer(compliment)
+    else:
+        if IMAGE_PATHS:
+            image_path = random.choice(IMAGE_PATHS)
+            photo = FSInputFile(image_path)  # Используем InputFile для корректной отправки
+            await message.answer_photo(photo=photo)
+                   
 # Запуск бота
 async def main():
     await dp.start_polling(bot)
