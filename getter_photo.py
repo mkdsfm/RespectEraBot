68ba7ade67d9d1cc53b5721f44c2c80cf6ff9500
Irina Tiulkina <irina.tyulkina10@mail.ru>
2025-02-23 17:19:29 +0300

    [fix]

--- getter_photo.py
+++ getter_photo.py
@@ -1,10 +1,12 @@
 import os
 import random
-from aiogram.types import FSInputFile
 
 IMAGE_DIR = "images"
 IMAGE_PATHS = [os.path.join(IMAGE_DIR, file) for file in os.listdir(IMAGE_DIR) if os.path.isfile(os.path.join(IMAGE_DIR, file))]
 
 async def get_random_photo_async():
+    # gigachad_photo = await getter_gigachad.get_gigachad_photo(bot, message.from_user.id)
+    # if gigachad_photo:
+    #     await message.answer_photo(photo=types.FSInputFile(gigachad_photo), caption=compliment)
     image_path = random.choice(IMAGE_PATHS)
-    return FSInputFile(image_path)
+    return image_path
